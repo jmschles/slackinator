c2b6dc5b861ebb0848472e6a9984071d4fd42d9f
Jamie Schlessinger <james.schlessinger@gmail.com>
2017-04-13 11:58:13 -0700

    how bout dis

--- lib/token_checker.ex
+++ lib/token_checker.ex
@@ -1,4 +1,9 @@
 defmodule Slackinator.TokenChecker do
+  @valid_tokens [
+    System.get_env("SM_SLACK_TOKEN"),
+    System.get_env("BOOTSY_SLACK_TOKEN")
+  ]
+
   defmodule InvalidTokenError do
     defexception message: "Invalid token", plug_status: 401
   end
@@ -11,17 +16,10 @@ defmodule Slackinator.TokenChecker do
   end
 
   defp verify_token!(conn) do
-    unless Enum.member?(valid_tokens, passed_token(conn)), do: raise InvalidTokenError
+    unless Enum.member?(@valid_tokens, passed_token(conn)), do: raise InvalidTokenError
   end
 
   defp passed_token(conn) do
     Plug.Conn.fetch_query_params(conn).params["token"]
-  end
-
-  defp valid_tokens do
-    [
-      System.get_env("SM_SLACK_TOKEN"),
-      System.get_env("BOOTSY_SLACK_TOKEN")
-    ]
   end
 end
